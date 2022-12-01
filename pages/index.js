7ae6b057b00573eb2e1e83a53ea55d58a027eaee
StepanLebedevu <stepan_leb@mail.ru>
2022-12-01 11:50:15 +0300

    Fixed text-color

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home({ /*exploreData, cardsData*/ }) {
 
 
   return (
-    <div className="">
+    <div className="bg-white text-gray-800">
       <Head>
         <title>AirBNB</title>
         <meta name="AirBnB" content="Created ob Next.JS by S.Lebedev" />
@@ -26,7 +26,7 @@ export default function Home({ /*exploreData, cardsData*/ }) {
       <Header />
       <Banner />
 
-      <main className='max-w-7xl mx-auto px-8 sm:px-16 bg-white'>
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
